20662ca3678a02db814948fb9027543a474b8d83
ANGAT SHAH <angatshah0511@Angats-MacBook-Pro.local>
2025-07-19 22:46:13 +0530

    Sem 3 Materials

---
+++ SEMESTER - 3/Java/DRINKS/Beverage.java
@@ -0,0 +1,8 @@
+package Cafe;
+
+public class Beverage {
+    public void prepare() {
+        System.out.println("Preparing a basic beverage.");
+    }
+}
+
---
+++ SEMESTER - 3/Java/DRINKS/Cafe/Coffee.java
@@ -0,0 +1,11 @@
+package Cafe;
+
+public class Coffee extends Beverage {
+    public void prepare() {
+        System.out.println("Brewing a cup of coffee.");
+    }
+    
+    public void addCream() {
+        System.out.println("Adding cream to the coffee.");
+    }
+}
