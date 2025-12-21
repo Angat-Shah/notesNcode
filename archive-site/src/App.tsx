306956842726233ed3f486c2f5c7daffa0ad9807
ANGAT SHAH <133506874+Angat-Shah@users.noreply.github.com>
2025-12-21 17:41:40 +0530

    Add basename to BrowserRouter in App component

--- archive-site/src/App.tsx
+++ archive-site/src/App.tsx
@@ -11,19 +11,19 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <ThemeProvider defaultTheme="system" storageKey="notesncode-ui-theme">
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        {/* ADD THE BASENAME HERE */}
+        <BrowserRouter basename="/notesNcode">
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
   </ThemeProvider>
 );
 
